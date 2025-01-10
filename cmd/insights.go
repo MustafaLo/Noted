8baa933af53219de1011f8c31f82307ca5a24b2a
MustafaLo <mustafalonandwala04@gmail.com>
2025-01-09 22:33:02 -0500

    Set up cohere logic

--- cmd/insights.go
+++ cmd/insights.go
@@ -4,6 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
@@ -11,6 +12,9 @@ import (
 	"github.com/MustafaLo/Noted/models"
 	"github.com/dstotijn/go-notion"
 	"github.com/spf13/cobra"
+
+	client "github.com/cohere-ai/cohere-go/v2/client"
+	cohere "github.com/cohere-ai/cohere-go/v2"
 )
 
 func getAllNotes(dbQueryResponse *notion.DatabaseQueryResponse)(string){
@@ -21,6 +25,26 @@ func getAllNotes(dbQueryResponse *notion.DatabaseQueryResponse)(string){
 	}
 	return note_block
 }
+
+func generateInsights(note_block string)(error){
+	envMap, err := internal.LoadEnv(); 
+	if err != nil{
+		return err
+	}
+
+	co := client.NewClient(client.WithToken(envMap["COHERE_API_KEY"]))	
+	message := fmt.Sprintf("Summarize these notes in 2-3 sentences or fewer, highlighting key insights:\n%s", note_block)
+	model_name := "command-r-plus-08-2024"
+
+	resp, err := co.Chat(
+		context.TODO(),
+		&cohere.ChatRequest{
+			Model: &model_name,
+			Message: message,
+		},
+	)
+	return nil
+}	
 
 var insightsCmd = &cobra.Command{
 	Use:   "insights",
@@ -48,7 +72,11 @@ var insightsCmd = &cobra.Command{
 			fmt.Printf("No insights found")
 			return
 		}
-		fmt.Println(notes)
+
+		if err := generateInsights(notes); err != nil{
+			fmt.Printf("Error %s", err)
+			return
+		}
 	},
 }
 
--- go.mod
+++ go.mod
@@ -5,7 +5,15 @@ go 1.23.4
 require github.com/spf13/cobra v1.8.1
 
 require (
-	github.com/dstotijn/go-notion v0.11.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.30.3 // indirect
+	github.com/aws/smithy-go v1.20.3 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
+)
+
+require (
+	github.com/cohere-ai/cohere-go/v2 v2.12.4
+	github.com/dstotijn/go-notion v0.11.0
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/joho/godotenv v1.5.1
 	github.com/spf13/pflag v1.0.5 // indirect
